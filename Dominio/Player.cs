e4002e158e4b9c8dc7e9764af184fca482df19b1
Voya1009 <gaz1009@gmail.com>
2022-09-14 20:22:02 -0300

    class Player edited

---
+++ Dominio/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Dominio
+{
+    public class Player
+    {
+        private int id;
+        private string name;
+        private int number;
+        private DateTime birthDate;
+        private bool rightFoot;
+        private int height;
+        private int marketValue;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public int Number { get => number; set => number = value; }
+        public DateTime BirthDate { get => birthDate; set => birthDate = value; }
+        public bool RightFoot { get => rightFoot; set => rightFoot = value; }
+        public int Height { get => height; set => height = value; }
+        public int MarketValue { get => marketValue; set => marketValue = value; }
+
+        public Player(int id, string name, int number, DateTime birthDate, bool rightFoot, int height, int marketValue)
+        {
+            this.id = id;
+            this.name = name;
+            this.number = number;
+            this.birthDate = birthDate;
+            this.rightFoot = rightFoot;
+            this.height = height;
+            this.marketValue = marketValue;
+        }
+
+       
+        public enum position { };       
+
+    }
+}
