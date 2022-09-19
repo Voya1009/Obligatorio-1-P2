579ed8cc71cc76d9708550888e6e9d50422557e4
Francisco Lopez <Flopezvaccaro@gmail.com>
2022-09-19 20:28:57 -0300

    Added abstract class Match

---
+++ Dominio/Match.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Team;
+
+namespace Dominio
+{
+    public abstract class Match
+    {
+        #region Atributes
+        private static counter = 0;
+        private int id;
+        private Team localTeam;
+        private Team visitingTeam;
+        private DateTime expectedDate;
+        private Boolean hasEnded;
+        //event list
+        private string result;
+        #endregion
+
+        #region properties
+        public int Id { get => id; set => id = value; }
+        public Team LocalTeam { get => localTeam; set => localTeam = value; }
+        public Team VisitingTeam { get => visitingTeam; set => visitingTeam = value; }
+        public DateTime ExpectedDate { get => expectedDate; set => expectedDate = value; }
+        public bool HasEnded { get => hasEnded; set => hasEnded = value; }
+        public string Result { get => result; set => result = value; }
+        #endregion
+
+        public Match(Team localTeam, Team visitingTeam, DateTime expectedDate)
+        {
+            this.id = ++this.counter;
+            this.localTeam = localTeam;
+            this.visitingTeam = visitingTeam;
+            this.expectedDate = expectedDate;
+            this.hasEnded = false;
+            this.result = "pendiente";
+        }
+
+        public abstract void finalizeMatch();
+
+    }
+}
